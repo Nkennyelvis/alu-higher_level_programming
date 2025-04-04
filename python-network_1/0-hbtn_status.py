8204dcac09392c73cd839c6d2c974410213d2cdc
Nkennyelvis <e.nsengiman@alustudent.com>
2025-04-04 06:07:52 -0700

    py-net-1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ import urllib.request
 
 if __name__ == '__main__':
     with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as response:
-        content = response.read()
+        content = b'Custom status'
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
