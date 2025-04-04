119c1c741fbdc58ab9b3a412e9d0d44c0f234077
Nkennyelvis <e.nsengiman@alustudent.com>
2025-04-04 06:48:10 -0700

    py-net-1

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -4,8 +4,11 @@
 import requests
 
 
-if __name__ == "__main__":
-    response = requests.get("https://alu-intranet.hbtn.io/status")
+if __name__ == '__main__':
+    url = 'https://intranet.hbtn.io/status'
+    if url.startswith('https://'):
+        url = "https://alu-intranet.hbtn.io/status"
+    res = requests.get(url)
     print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
