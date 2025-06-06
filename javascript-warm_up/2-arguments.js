0bb28ddb9c059e48b593729fe42cdb03c2b43643
Nkennyelvis <e.nsengiman@alustudent.com>
2025-06-06 15:56:27 +0200

    js warm up

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,2 +1,13 @@
 #!/usr/bin/node
 
+const args = process.argv;
+
+const numberOfArguments = args.length - 2;
+
+if (numberOfArguments === 0) {
+  console.log("No argument");
+} else if (numberOfArguments === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
