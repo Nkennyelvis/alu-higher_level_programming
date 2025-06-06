5b98ad96cd00d3ff57584abd5b961f3faf03076d
Nkennyelvis <e.nsengiman@alustudent.com>
2025-06-06 15:39:52 +0200

    js warm up

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const FirstLine = 'C is fun';
+const SecondLine = 'Python is cool';
+const ThirdLine = 'JavaScript is amazing';
+
+console.log(FirstLine);
+console.log(SecondLine);
+console.log(ThirdLine);
